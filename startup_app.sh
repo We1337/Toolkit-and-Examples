b17ab1c32fc71e0e1ed263de387c7f9790f3f7e6
We1337 <githubdevice@gmail.com>
2023-10-19 23:59:19 +0600

    feater: new links for google-chrome

--- startup_app.sh
+++ startup_app.sh
@@ -1,2 +1,2 @@
 code
-google-chrome https://github.com https://gitlab.com https://wakatime.com
+google-chrome https://github.com https://gitlab.com https://codeberg.org https://bitbucket.org https://wakatime.com https://codetime.dev
